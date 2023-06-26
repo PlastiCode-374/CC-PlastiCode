bd0d6977dfe3310b54e3c39e34113b3b1c7a8c55
Achmad <ahmad.aditiansah77@gmail.com>
2023-06-26 16:51:06 +0700

    fixed db

--- src/app/Http/Controllers/API/ImageController.php
+++ src/app/Http/Controllers/API/ImageController.php
@@ -9,15 +9,10 @@ use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
 use App\Models\History;
 use Illuminate\Validation\Rule;
+use Carbon\Carbon;
 
 class ImageController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->headers->set('Content-Type', 'multipart/form-data');
@@ -32,7 +27,9 @@ class ImageController extends Controller
         }
 
         $image = $request->file('file');
-        $filePath = 'user/' . $image->getClientOriginalName();
+        $currentDateTime = Carbon::now()->format('Ymd_His');
+        $fileName = $currentDateTime . '_' . $image->getClientOriginalName();
+        $filePath = 'user/' . $fileName;
         Storage::disk('gcs')->put($filePath, file_get_contents($image), 'public');
         $imageUrl = env('GCS_STORAGE_API_URI').'/'.env('GCS_BUCKET').'/'.$filePath;
 
@@ -42,7 +39,6 @@ class ImageController extends Controller
         $history->url_image = $imageUrl;
         $history->save();
 
-//      $this->uploadFile($request->file('file'), 'ImageHasil');
         return response()->json([
             'status_code' => 201,
             'success' => true,
@@ -52,81 +48,4 @@ class ImageController extends Controller
             'image_url' => $imageUrl
         ]);
     }
-
-    private function uploadFile(UploadedFile $file, $folder = null, $filename = null)
-    {
-        $name = !is_null($filename) ? $filename : Str::random(25);
-
-        return $file->storeAs(
-            $folder,
-            $name . "." . $file->getClientOriginalExtension(),
-            'gcs'
-        );
-    }
-
-    public function update(Request $request, $id)
-{
-    $request->validate([
-        'jenis_plastik' => 'required',
-        'masa_pakai' => 'required',
-        'tingkat_bahaya' => 'required',
-        'detail_jenis_plastik' => 'required',
-        'detail_masa_pakai' => 'required',
-        'detail_tingkat_bahaya' => 'required',
-    ]);
-
-    $history = History::find($id);
-    if(!$history) {
-        return response()->json([
-            'status_code' => 404,
-            'success' => false,
-            'error' => true,
-            'error_message' => 'History not found'
-        ]);
-    }
-
-    // Memeriksa apakah pengguna yang dikaitkan dengan history adalah pengguna yang mengirim permintaan
-    if ($request->user()->id !== $history->user_id) {
-        return response()->json([
-            'status_code' => 403,
-            'success' => false,
-            'error' => true,
-            'error_message' => 'You are not authorized to update this history'
-        ]);
-    }
-
-    tap($history)->update($request->except('history_id'));
-
-    return response()->json([
-        'status_code' => 201,
-        'success' => true,
-        'error' => false,
-        'success_message' => 'History has been updated successfully',
-        'data' => $history
-    ]);
-}
-
-
-public function index(Request $request, $user_id)
-{
-    $history = History::where('user_id', $user_id)->get();
-
-    if (!$history->isEmpty()) {
-        return response()->json([
-            'status_code' => 200,
-            'success' => true,
-            'error' => false,
-            'success_message' => 'Histories found',
-            'histories' => $history
-        ]);
-    }
-
-    return response()->json([
-        'status_code' => 404,
-        'success' => false,
-        'error' => true,
-        'error_message' => 'History with user ID ' . $user_id . ' not found',
-        'histories' => $history
-    ]);
-}
 }
